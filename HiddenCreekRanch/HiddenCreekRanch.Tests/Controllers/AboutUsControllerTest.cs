8c7af2da0b96c68046b0be26e8c2a6b4e3cfa50b
Joseph Moore <joemo65@gmail.com>
2018-01-24 21:19:04 -0800

    added nuget packages and created base foundation for project

--- HiddenCreekRanch/HiddenCreekRanch.Tests/Controllers/AboutUsControllerTest.cs
+++ HiddenCreekRanch/HiddenCreekRanch.Tests/Controllers/AboutUsControllerTest.cs
@@ -1,21 +1,17 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Web.Mvc;
-using HiddenCreekRanch;
 using HiddenCreekRanch.Controllers;
+using NUnit.Framework;
 
 namespace HiddenCreekRanch.Tests.Controllers
 {
     [TestFixture]
-    public class HomeControllerTest
+    public class AboutUsControllerTest
     {
         [Test]
         public void Index_Index_ViewName_Assert_AreEqual()
         {
-            var controller = new HomeController();
+            var controller = new AboutUsController();
             var result = controller.Index() as ViewResult;
             Assert.AreEqual("Index", result.ViewName);
         }
--- HiddenCreekRanch/HiddenCreekRanch/Controllers/HomeController.cs
+++ HiddenCreekRanch/HiddenCreekRanch/Controllers/HomeController.cs
@@ -7,17 +7,12 @@ using System.Web.Mvc.Ajax;
 
 namespace HiddenCreekRanch.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            var mvcName = typeof(Controller).Assembly.GetName();
-            var isMono = Type.GetType("Mono.Runtime") != null;
-
-            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
-            return View();
+           return View("Index");
         }
     }
 }
