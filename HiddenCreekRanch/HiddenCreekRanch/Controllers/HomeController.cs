9280185cf563da4f9a19a5a6e8f3aa8a1ea4e77e
Joseph Moore <joemo65@gmail.com>
2018-01-25 21:33:00 -0800

    created .NET CORE mvc site to be able to publish to azure :+1:

--- HiddenCreekRanch/HiddenCreekRanch/Controllers/HomeController.cs
+++ HiddenCreekRanch/HiddenCreekRanch/Controllers/HomeController.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HiddenCreekRanch.Models;
 
 namespace HiddenCreekRanch.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
-        [HttpGet]
-        public ActionResult Index()
+        public IActionResult Index()
         {
-           return View("Index");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ HiddenCreekRanch/HiddenCreekRanch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HiddenCreekRanch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
